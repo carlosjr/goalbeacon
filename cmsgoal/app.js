78b9ee66d234c6aeb112016129f3e66eb03f2e93
Carlos Jr <carlosjrvieira14@gmail.com>
2017-03-20 20:42:06 -0300

    criacao incial home

--- cmsgoal/app.js
+++ cmsgoal/app.js
@@ -12,14 +12,14 @@ app.config(function($routeProvider) {
         controller: 'BeaconsCtrl'
       })
       .when('/',{
-        templateUrl: 'partial/main.html',
-        //controller: 'MainCtrl'
+        templateUrl: 'partial/users.html',
+        controller: 'CustomersCtrl'
       });
 
     //$locationProvider.html5Mode(true);
 })
 .controller('OffersCtrl', ['$scope','$route', '$routeParams', '$location',
-  function OffersCtrl($route, $routeParams, $location) {
+  function OffersCtrl($scope, $route, $routeParams, $location) {
     this.$route = $route;
     this.$location = $location;
     this.$routeParams = $routeParams;
@@ -44,14 +44,14 @@ app.config(function($routeProvider) {
 
 
 }])
-.controller('BeaconsCtrl', ['$scope','$routeParams', function BeaconsCtrl($routeParams) {
+.controller('BeaconsCtrl', ['$scope','$routeParams', function BeaconsCtrl($scope,$routeParams) {
   this.name = 'BeaconsCtrl';
   this.params = $routeParams;
   alert("beacon");
   $scope.listarBeacons = function(){
     $http({
       method: 'GET',
-      url: '/someUrl'
+      url: 'http://localhost:3000/api/beacons'
     }).then(function successCallback(response) {
       // this callback will be called asynchronously
       // when the response is available
@@ -63,5 +63,33 @@ app.config(function($routeProvider) {
     });
 
   };
+}])
+.controller('CustomersCtrl', ['$scope','$routeParams', '$http',function CustomersCtrl($scope,$routeParams,$http){
+
+  $scope.salvarCliente = function(){
+    var customer = {
+      name:"Lorrana Ellen",
+      email:"lo-ellen@hotmail.com",
+      mobile:"34988361052"
+    };
+
+    $http({
+      method: 'POST',
+      data: customer,
+      url: 'http://localhost:3000/api/customers'
+    }).then(function successCallback(response) {
+      // this callback will be called asynchronously
+      // when the response is available
+      $scope.customer = response.data;
+      alert(response);
+    }, function errorCallback(response) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      if(response.error != false){
+        alert("Erro do rest:"+response.message);
+      }
+      alert("Erro:"+response);
+    });
+  }
 }])
 ;
